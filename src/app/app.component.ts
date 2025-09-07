16d7ecb9d4d4c67d2c1b6d5e8a78df85403624bf
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2025-09-07 22:06:25 +0300

    fix: lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, WritableSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  WritableSignal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { completeIconSet } from '@icons/tm-icons';
 import { TmIconsService } from '@icons/tm-icons.service';
@@ -17,7 +21,8 @@ import { MainLoaderService } from '@core/services';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  public loaderSignal: WritableSignal<boolean> = this.mainLoaderService.getLoaderState();
+  public loaderSignal: WritableSignal<boolean> =
+    this.mainLoaderService.getLoaderState();
 
   constructor(
     private epIconsService: TmIconsService,
